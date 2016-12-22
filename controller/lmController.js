e1beab0156d025027e6f7b5ca8a9c213d4fa6c0f
MengXJ <907564255@qq.com>
2016-12-22 17:22:10 +0800

    1222
    ？

--- controller/lmController.js
+++
@@ -1,6 +0,0 @@
-var app = angular.module('lmApp',[]);
-app.controller('lmCtrl',function($scope){
-	$scope.urltop = '../resources/nav.html';
-	$scope.urlpartnertop = '../resources/partner_nav.html';
-	$scope.urlfooter = '../resources/footer.html';
-})
